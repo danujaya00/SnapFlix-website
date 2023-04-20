213a77540886a43bfeba4be5c207bd68fb36852e
Danujaya  Liyanage <2021is054@stu.ucsc.cmb.ac.lk>
2023-04-20 22:42:40 +0530

    added footer and fixed minor bugs

--- js/hero.js
+++ js/hero.js
@@ -30,12 +30,12 @@ var messenger1 = function(el){
   var m = this;
   
   m.init = function(){
-    m.codeletters = "as dfg hj jalfa";
+    m.codeletters = "a ds asd";
     m.message = 0;
     m.current_length = 0;
     m.fadeBuffer = false;
     m.messages = [
-      'Sri lankas First-Ever Short film Hackathon, that creates a platform to guide and encourage SriLankan youth to participate in international competitions and make our country shine on a global stage.',
+      'Sri lankas First-Ever Short film Hackathon, that creates a platform to guide and encourage SriLankan youth',
     ];
     
     
@@ -118,3 +118,98 @@ var messenger1 = function(el){
 
 console.clear();
 var messenger1 = new messenger1($('#messenger1'));
+
+
+// var messenger2 = function(el){
+//   'use strict';
+//   var m = this;
+  
+//   m.init = function(){
+//     m.codeletters = "as dfg hj jalfa";
+//     m.message = 0;
+//     m.current_length = 0;
+//     m.fadeBuffer = false;
+//     m.messages = [
+//       'international competitions and make our country shine on a global stage.',
+//     ];
+    
+    
+
+
+//     setTimeout(m.animateIn, 1000);
+//   };
+  
+//   m.generateRandomString = function(length){
+//     var random_text = '';
+//     while(random_text.length < length){
+//       random_text += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
+//     } 
+    
+//     return random_text;
+//   };
+  
+//   m.animateIn = function(){
+//     if(m.current_length < m.messages[m.message].length){
+//       m.current_length = m.current_length + 2;
+//       if(m.current_length > m.messages[m.message].length) {
+//         m.current_length = m.messages[m.message].length;
+//       }
+      
+//       var message = m.generateRandomString(m.current_length);
+//       $(el).html(message);
+      
+//       setTimeout(m.animateIn, 20);
+//     } else { 
+//       setTimeout(m.animateFadeBuffer, 20);
+//     }
+//   };
+  
+//   m.animateFadeBuffer = function(){
+//     if(m.fadeBuffer === false){
+//       m.fadeBuffer = [];
+//       for(var i = 0; i < m.messages[m.message].length; i++){
+//         m.fadeBuffer.push({c: (Math.floor(Math.random()*12))+1, l: m.messages[m.message].charAt(i)});
+//       }
+//     }
+    
+//     var do_cycles = false;
+//     var message = ''; 
+    
+//     for(var i = 0; i < m.fadeBuffer.length; i++){
+//       var fader = m.fadeBuffer[i];
+//       if(fader.c > 0){
+//         do_cycles = true;
+//         fader.c--;
+//         message += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
+//       } else {
+//         message += fader.l;
+//       }
+//     }
+    
+//     $(el).html(message);
+    
+//     if(do_cycles === true){
+//       setTimeout(m.animateFadeBuffer, 100);
+//     } else {
+//       setTimeout(m.cycleText,80000);
+//     }
+//   };
+  
+//   m.cycleText = function(){
+//     m.message = m.message + 1;
+//     if(m.message >= m.messages.length){
+//       m.message = 0;
+//     }
+    
+//     m.current_length = 0;
+//     m.fadeBuffer = false;
+//     $(el).html('');
+    
+//     setTimeout(m.animateIn, 200);
+//   };
+  
+//   m.init();
+// }
+
+// console.clear();
+// var messenger2 = new messenger2($('#messenger2'));
