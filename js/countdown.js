1939871075f0e6e4ea5bf9cbc9ccbf61bec94687
raviendalpatadu <102800797+raviendalpatadu@users.noreply.github.com>
2023-04-21 22:40:34 +0530

    contact us new modifications

--- js/countdown.js
+++ js/countdown.js
@@ -1,19 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  // Unix timestamp (in seconds) to count down to
 
-    // Unix timestamp (in seconds) to count down to
-    var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1;
-  
-    // Set up FlipDown
-    var flipdown = new FlipDown(twoDaysFromNow)
-  
-      // Start the countdown
-      flipdown.start()
-  
-      // Do something when the countdown ends
-      flipdown.ifEnded(() => {
-        console.log('The countdown has ended!');
-      });
-    
-    var ver = document.getElementById('ver');
-    ver.innerHTML = flipdown.version;
-  });  
+  var customDate = new Date("2023-05-14");
+  var now = new Date().getTime();
+  var timeDiff = customDate.getTime() - now;
+  var seconds = Math.floor(timeDiff / 1000);
+  var twoDaysFromNow = new Date().getTime() / 1000 + seconds - 19800;
+
+  // Set up FlipDown
+  var flipdown = new FlipDown(twoDaysFromNow);
+
+  // Start the countdown
+  flipdown.start();
+
+  // Do something when the countdown ends
+  flipdown.ifEnded(() => {
+    console.log("The countdown has ended!");
+  });
+
+  var ver = document.getElementById("ver");
+  ver.innerHTML = flipdown.version;
+});
